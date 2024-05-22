f3ca411bfedd7a4eda8f1dd5ae3a6b95303ec933
maqsoodhussain <maqsoodhussain@gdcbeerwah.edu.in>
2024-05-22 23:45:58 +0530

    basic bootstrap code

---
+++ login.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://unpkg.com/bs-brain@2.0.4/components/registrations/registration-4/assets/css/registration-4.css">
+    <title>Document</title>
+</head>
+<body>
+ <!-- Registration 4 - Bootstrap Brain Component -->
+<section class="p-3 p-md-4 p-xl-5 " style="background-color: #D8DFEE;">
+  <div class="container">
+    <div class="card border-light-subtle shadow-sm">
+      <div class="row g-0">
+        <div class="col-12 col-md-6">
+          <img class="img-fluid rounded-start w-100 h-100 object-fit-cover" loading="lazy" src="https://bootstrapbrain.com/demo/components/registrations/registration-4/assets/img/logo-img-1.webp" alt="BootstrapBrain Logo">
+        </div>
+        <div class="col-12 col-md-6">
+          <div class="card-body p-3 p-md-4 p-xl-5">
+            <div class="row">
+              <div class="col-12">
+                <div class="mb-5">
+                  <h2 class="h3">Login</h2>
+                  <h3 class="fs-6 fw-normal text-secondary m-0">Enter your Username and Password</h3>
+                </div>
+              </div>
+            </div>
+            <form action="#!">
+              <div class="row gy-3 gy-md-4 overflow-hidden">
+                <div class="col-12">
+                  <label for="username" class="form-label">Username <span class="text-danger">*</span></label>
+                  <input type="text" class="form-control" name="username" id="username" placeholder="Enter Username" required>
+                </div>
+                
+                <div class="col-12">
+                  <label for="password" class="form-label">Password <span class="text-danger">*</span></label>
+                  <input type="password" class="form-control" name="password" id="password" value="" required>
+                </div>
+                
+                <div class="col-12">
+                  <div class="d-grid">
+                    <button class="btn bsb-btn-xl btn-primary" type="submit">Sign up</button>
+                  </div>
+                </div>
+              </div>
+            </form>
+            <div class="row">
+              <div class="col-12">
+                <hr class="mt-5 mb-4 border-secondary-subtle">
+                <p class="m-0 text-secondary text-center">Don't have an account? <a href="signup.php" class="link-primary text-decoration-none">Sign Up</a></p>
+              </div>
+            </div>
+        
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+      
+
+
+</body>
+</html>
