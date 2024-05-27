55d54f5a828e35e346ca66d88601d391eb85d0be
maqsoodhussain <maqsoodhussain@gdcbeerwah.edu.in>
2024-05-27 12:18:19 +0530

    #

--- handle.php
+++ handle.php
@@ -16,22 +16,31 @@
      $name =$_POST["name"];
      $email =$_POST["email"];
      $password = $_POST["password"];
-     
+     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+    
+     $query = "Insert into logindetails(username, name, email, password) values('$uname', '$name','$email','$hashedPassword')";
+
+    if(!$con){echo("No connection");}else{
+        $res = mysqli_query($con,$query);
+
+    }
+
     echo '<script type="text/JavaScript"> alert("your details are submited successfully ");</script>';
 
-    // echo "<script>window.location.href = 'login.php';</script>";
+    echo "<script>window.location.href = 'login.php';</script>";
 
-     echo "details are: \n";
-     echo $uname,"<br>";
-     echo $name,"<br>";
-     echo $email,"<br>";
-     echo $password,"<br>";
-     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-     echo $hashedPassword,"<br>";
+    //  echo "details are: \n";
+    //  echo $uname,"<br>";
+    //  echo $name,"<br>";
+    //  echo $email,"<br>";
+    //  echo $password,"<br>";
+     
+    //  echo $hashedPassword,"<br>";
      
     
      
-    
+    $con->close();
      ?>
 
 
