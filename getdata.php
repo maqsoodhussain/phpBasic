fbeb9451d4bbffb59f60f364ec17e965f4a3b9d6
maqsoodhussain <maqsoodhussain@gdcbeerwah.edu.in>
2024-06-01 20:46:04 +0530

    #

--- getdata.php
+++ getdata.php
@@ -1,7 +1,27 @@
 <?php
             require("connection.php");
 
+            $db_data = "Select * from logindetails";
 
-            $db_data = "Select * from login logindetails";
+            $result = mysqli_query($con, $db_data);
+            echo mysqli_num_rows($result);
+            
+            if (mysqli_num_rows($result) > 0) {
+
+              
+              while($row = mysqli_fetch_assoc($result)) {
+                echo 
+                "<br>USERNAME: " . $row["username"]. 
+                "<br>NAME: " . $row["name"]. 
+                "<br>EMAIL: " . $row["email"].
+                "<br>PASSWORD: " . $row["password"];
+                 
+              }
+            } else {
+              echo "0 results";
+            }
+
+
+            mysqli_close($con);
 
 ?>
